ab75afd44fbca2f4bbd0a54165764399f0d7eff4
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-08-24 11:28:34 +0530

    5 products in the cart

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+//Importing Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+//App component's style sheet
+import "./App.css";
+
+//Importing other child components
+import ContextComponent from "./ContextComponent";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import ProductList from "./Components/ProductList";
+
+function App() {
+  return (
+    <>
+      {/* wrapping the child component which going to use context inside the ContextComponent*/}
+      <ContextComponent>
+        <Header />
+        <ProductList />
+      </ContextComponent>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,100 @@
+const data = [
+  {
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail:
+      "https://www.apple.com/newsroom/images/product/iphone/standard/iphonex_front_back_glass_big.jpg.large_2x.jpg",
+    images: [
+      "https://www.apple.com/newsroom/images/product/iphone/standard/iphonex_front_back_glass_big.jpg.large_2x.jpg",
+      "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/850360/pexels-photo-850360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/1156684/pexels-photo-1156684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+  {
+    id: 2,
+    title: "iPhone X",
+    description:
+      "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    price: 899,
+    discountPercentage: 17.94,
+    rating: 4.44,
+    stock: 34,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail:
+      "https://images.pexels.com/photos/1156684/pexels-photo-1156684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    images: [
+      "https://images.pexels.com/photos/1156684/pexels-photo-1156684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/4911253/pexels-photo-4911253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/2740955/pexels-photo-2740955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/5053987/pexels-photo-5053987.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+  {
+    id: 3,
+    title: "Samsung Universe 9",
+    description:
+      "Samsung's new variant which goes beyond Galaxy to the Universe",
+    price: 1249,
+    discountPercentage: 15.46,
+    rating: 4.09,
+    stock: 36,
+    brand: "Samsung",
+    category: "smartphones",
+    thumbnail:
+      "https://images.pexels.com/photos/11772518/pexels-photo-11772518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    images: [
+      "https://images.pexels.com/photos/11772518/pexels-photo-11772518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+  {
+    id: 4,
+    title: "OPPOF19",
+    description: "OPPO F19 is officially announced on April 2021.",
+    price: 280,
+    discountPercentage: 17.91,
+    rating: 4.3,
+    stock: 123,
+    brand: "OPPO",
+    category: "smartphones",
+    thumbnail:
+      "https://images.pexels.com/photos/20895977/pexels-photo-20895977/free-photo-of-oppo-phone-against-blue-background-commercial.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    images: [
+      "https://images.pexels.com/photos/20895988/pexels-photo-20895988/free-photo-of-oppo-find-x7-phone-next-to-books-and-tablet-on-desk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/799443/pexels-photo-799443.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+  {
+    id: 5,
+    title: "Huawei P30",
+    description:
+      "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    price: 499,
+    discountPercentage: 10.58,
+    rating: 4.09,
+    stock: 32,
+    brand: "Huawei",
+    category: "smartphones",
+    thumbnail:
+      "https://images.pexels.com/photos/3791664/pexels-photo-3791664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    images: [
+      "https://images.pexels.com/photos/3791664/pexels-photo-3791664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3791664/pexels-photo-3791664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/3791664/pexels-photo-3791664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    ],
+  },
+];
+
+export { data };
